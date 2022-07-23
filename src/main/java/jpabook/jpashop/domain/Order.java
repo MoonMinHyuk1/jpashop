8c8d342bd4b9d82639135465d88bd903026d6d2a
MoonMinHyuk1 <moon981018@gmail.com>
2022-07-24 02:48:37 +0900

    2-3

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id") //연관관계의 주인일때 (외래키가 있는 테이블)
+    private Member member;
+
+    @OneToMany(mappedBy = "order")  //연관관계의 주인이 아닐때 (외래키가 없는 테이블)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate; //주문시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status; //주문상태 [ORDER, CANCEL]
+
+}
