d3efd542e5b5a160ccd8763d6285236dbbda0fce
MoonMinHyuk1 <moon981018@gmail.com>
2022-07-29 16:01:58 +0900

    update

--- src/main/java/jpabook/jpashop/domain/item/Album.java
+++ src/main/java/jpabook/jpashop/domain/item/Album.java
@@ -7,7 +7,7 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
-@DiscriminatorValue("AA")
+@DiscriminatorValue("A")
 @Getter @Setter
 public class Album extends Item {
 
